e4b380dfadcb15b20a2f9980bfb9ef8727f752c6
Chloe Steffens <caforonda103@yahoo.com>
2020-10-05 20:33:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
-});
-
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
+  document.getElementById("text").innerHTML = "This is really cool!";
+})
